1f98d46b68dd762d30ad3ab65623255ff5b309c4
italokrenak <italokrenak@gmail.com>
2018-02-09 19:03:51 -0300

    Adicionando diversas configurações para testar diversas possibilidades de acessar profiles através do Config Server

--- lab-4-sentence/src/main/java/demo/SentenceController.java
+++ lab-4-sentence/src/main/java/demo/SentenceController.java
@@ -18,8 +18,10 @@ public class SentenceController {
 
 	@GetMapping("/sentence")
 	public String getSentence() {
-		return getWord("LAB-4-SUBJECT") + " " + getWord("LAB-4-VERB") + " " + getWord("LAB-4-ARTICLE") + " "
+		return getWord("LAB-4-SUBJECT") + " " + getWord("LAB") + " " + getWord("LAB-4-ARTICLE") + " "
 				+ getWord("LAB-4-ADJECTIVE") + " " + getWord("LAB-4-NOUN") + ".";
+//		return getWord("LAB-4-SUBJECT") + " " + getWord("LAB-4-VERB") + " " + getWord("LAB-4-ARTICLE") + " "
+//				+ getWord("LAB-4-ADJECTIVE") + " " + getWord("LAB-4-NOUN") + ".";
 
 	}
 
